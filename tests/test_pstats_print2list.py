fbb2628009bb2088187ab298565f45681e0e6442
Hugo Adan [Vauxoo] <hugo@vauxoo.com>
2017-03-14 00:55:59 -0500

    [FIX] test: Update results length (#12)
    * [FIX] Update results length
    
    * [FIX] extend exclude_fnames to discard profile and python values

--- tests/test_pstats_print2list.py
+++ tests/test_pstats_print2list.py
@@ -48,13 +48,14 @@ class TestPstatsPrint2list(unittest.TestCase):
 
     def test_030_exclude(self):
         result = get_pstats_print2list(
-            self.fstats_fib,
-            filter_fnames=None, exclude_fnames='seq', limit=None)
+            self.fstats_fib, filter_fnames=None,
+            exclude_fnames=['seq', '>', 'profile'], limit=None)
         self.assertEqual(len(result), 1)
         self.assertEqual(os.path.basename(result[0]['file']), 'fib.py')
 
     def test_040_none(self):
-        result = get_pstats_print2list(self.fstats_fib)
+        result = get_pstats_print2list(
+            self.fstats_fib, exclude_fnames=['>', 'profile'])
         self.assertEqual(len(result), 2)
 
     def test_050_filter_exclude(self):
@@ -67,7 +68,7 @@ class TestPstatsPrint2list(unittest.TestCase):
         result = get_pstats_print2list(
             self.fstats_fib,
             filter_fnames=[], exclude_fnames=None, limit=None)
-        self.assertEqual(len(result), 2)
+        self.assertEqual(len(result), 4)
 
     def test_060_fnonexistent(self):
         result = get_pstats_print2list(
@@ -94,27 +95,31 @@ class TestPstatsPrint2list(unittest.TestCase):
 
     def test_090_sort_cumulative(self):
         result = get_pstats_print2list(
-            self.fstats_fib, sort='cumulative')
+            self.fstats_fib, sort='cumulative',
+            exclude_fnames=['>', 'profile'])
         self.assertEqual(len(result), 2)
         self.assertEqual(os.path.basename(result[0]['file']), 'fib_seq.py')
         self.assertEqual(os.path.basename(result[1]['file']), 'fib.py')
 
     def test_100_sort_calls(self):
-        result = get_pstats_print2list(self.fstats_fib, sort='calls')
+        result = get_pstats_print2list(
+            self.fstats_fib, sort='calls', exclude_fnames=['>', 'profile'])
         self.assertEqual(len(result), 2)
         self.assertEqual(os.path.basename(result[0]['file']), 'fib.py')
         self.assertEqual(os.path.basename(result[1]['file']), 'fib_seq.py')
 
     def test_110_sort_calls_reverse(self):
         result = get_pstats_print2list(
-            self.fstats_fib, sort='calls', sort_reverse=True)
+            self.fstats_fib, sort='calls', sort_reverse=True,
+            exclude_fnames=['>', 'profile'])
         self.assertEqual(len(result), 2)
         self.assertEqual(os.path.basename(result[0]['file']), 'fib_seq.py')
         self.assertEqual(os.path.basename(result[1]['file']), 'fib.py')
 
     def test_120_sort_cumulative_reverse(self):
         result = get_pstats_print2list(
-            self.fstats_fib, sort='cumulative', sort_reverse=True)
+            self.fstats_fib, sort='cumulative', sort_reverse=True,
+            exclude_fnames=['>', 'profile'])
         self.assertEqual(len(result), 2)
         self.assertEqual(os.path.basename(result[0]['file']), 'fib.py')
         self.assertEqual(os.path.basename(result[1]['file']), 'fib_seq.py')
